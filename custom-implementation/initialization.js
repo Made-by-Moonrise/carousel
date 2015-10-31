45b58b1be47c5e37aa5d0cf4aaab662b93ad8a44
Jameson Nyeholt <jnyeholt@jamasoftware.com>
2015-10-31 13:03:54 -0700

    update initialization to use debounced call

--- custom-implementation/initialization.js
+++ custom-implementation/initialization.js
@@ -1,5 +1,17 @@
 jQuery(function ($) {
-    $("#yourCarouselId").touchCarousel({
-        /* carousel options go here see Javascript Options section for more info */
+    $(document).ready(function () {
+        var debounceRate = 500;
+
+        var debouncedCall = _.debounce(function () {
+            console.log('resize-timer');
+            //initializeTouchCarousel();
+            //initializeCaroufredsel();
+        }, debounceRate);
+
+        $(window).resize(debouncedCall);
     });
+
+    //$("#yourCarouselId").touchCarousel({
+    //    /* carousel options go here see Javascript Options section for more info */
+    //});
 });
