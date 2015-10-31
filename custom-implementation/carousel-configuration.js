d630b62b3219629b70051f4969bf82a91ba3e823
Jameson Nyeholt <jnyeholt@jamasoftware.com>
2015-10-31 12:47:51 -0700

    break out scripts into separate files

---
+++ custom-implementation/carousel-configuration.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jnyeholt on 10/31/15.
+ */
---
+++ custom-implementation/initialization.js
@@ -0,0 +1,5 @@
+jQuery(function ($) {
+    $("#yourCarouselId").touchCarousel({
+        /* carousel options go here see Javascript Options section for more info */
+    });
+});
